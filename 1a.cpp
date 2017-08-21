fbde45f39b9b9a52a5236ede4dd6ebeedeb822d8
IgorGorbunov20 <isgorbunov.spb@gmail.com>
2017-08-21 14:57:16 +0300

    changed 1a.cpp due to comments of the review.

--- 1a.cpp
+++ 1a.cpp
@@ -4,23 +4,24 @@ using namespace std;
 
 int main()
 {
-	vector<int> v1(5);
+	cout<<"Введите размер вектора\n";
+	int vecsize;
+	cin>> vecsize;
+	vector<int> v1(vecsize);
 	cout <<"Размер вектора: " <<v1.size()<<"\n";
 	cout <<"Коллекция чисел: \n";
-	int i;
-	for(i=4; i>=0; i--)
+	int counter=0;
+	for(int i=0; i<vecsize; i++)
 	{
-		int c=0;
-		v1[i]=i*2;		
-		cout<<c+1<<"е число "<<v1[i]<<"\n";
-		c=c+1;
+		v1.push_back((vecsize-counter)*2);		
+		cout<<counter+1<<"е число "<<v1.back()<<"\n";
+		counter=counter+1;
 	}
-	cout <<"Сортировка\n";
-	for (int j=0;j<v1.size();j++)
+	for (int j=vecsize;j<vecsize*2;j++)
 	{
 		int posit=j;
 		int temp=v1[posit];
-		for (int k=j+1;k<v1.size();k++)
+		for (int k=j+1;k<vecsize*2;k++)
 		{
 			if(v1[k]<temp)
 			{
@@ -31,9 +32,11 @@ int main()
 		v1[posit]=v1[j];
 		v1[j] = temp;
 	}
-	for (int p=0;p<v1.size();p++)
+
+	cout <<"Сортировка завершена\n";
+	for (int p=vecsize;p<vecsize*2;p++)
 	{
-		cout << v1[p]<<"\n";	
+		cout << "v1[p]: "<<v1[p]<<"\n";	
 	}
 	return 0;
 }
